3f70210aa2020ba8051e048c9f480249662b391d
mdanys <mochammaddany@gmail.com>
2022-09-26 17:38:00 +0700

    Add connection gorm

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func connectGorm() (*gorm.DB, error) {
+	dsn := "root:@tcp(127.0.0.1:3306)/rent-book?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func main() {
+
+}
