3c220e1d037299b9f08506b02830a969cb7996f2
mfaishal882 <mfaishal882@gmail.com>
2022-09-28 11:19:28 +0700

    done controller model lend

--- models/lends.go
+++ models/lends.go
@@ -9,24 +9,50 @@ import (
 
 type Lends struct {
 	gorm.Model
-	Return_date time.Time
-	Is_returned bool
-	IDUser      uint
-	IDBook      uint
+	Return_date        time.Time
+	Is_returned        bool
+	Book_owner_id      uint
+	Book_owner_email   string
+	Book_owner_name    string
+	Book_owner_address string
+	Book_owner_phone   string
+	Book_title         string
+	Book_author        string
+	IDUser             uint
+	IDBook             uint
 }
 
 type LendsModel struct {
 	DB *gorm.DB
 }
 
-func (lm LendsModel) GetUserBorrow(_IDUser uint) ([]Books, error) {
-	var result []Books
-	err := lm.DB.Where(&Books{IDUser: _IDUser}).Find(&result).Error
+// Fungsi menampilkan list buku yang user pinjam
+func (lm LendsModel) GetUserBookBorrow(_IDUser uint) ([]Lends, error) {
+	var result []Lends
+	err := lm.DB.Where(&Lends{IDUser: _IDUser}).Find(&result).Error
 	if err != nil {
 		fmt.Println("Error on Query", err.Error())
 		return nil, err
 	}
 	return result, nil
+}
+
+// Fungsi menambah peminjaman buku
+func (lm LendsModel) AddLend(_newLend Lends) (Lends, error) {
+	err := lm.DB.Save(&_newLend).Error
+	if err != nil {
+		return Lends{}, err
+	}
+	return _newLend, nil
+}
+
+// Fungsi Mengembalikan Buku
+func (lm LendsModel) ReturnBook(_lendData Lends) (Lends, error) {
+	err := lm.DB.Save(_lendData).Error
+	if err != nil {
+		return Lends{}, err
+	}
+	return _lendData, nil
 }
 
 // type BooksUsers struct {
